a46adab03526d84121d1039e267a3b2eea40fe1b
Andre-Junior <andrejunior.dias@gmail.com>
2021-04-23 14:01:25 -0300

    adc axios, updata com next
    Adicionou a ferramenta axios e adicionou com o next a chegada dos dados em json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+//tipagem Types
+type Episode = Array <{
+    id: string;
+    title: string;
+    members: string;
+}> //precisa passar qual o formato de oq é 
 
 
-export default function Home() {
+type HomeProps = {
+  episodes: Episode[]; //outra forma de declarar array
+}
+
+
+
+export default function Home(props: HomeProps) {
   return (
     <h1>Index</h1> 
   )
 }
+
+
+//Next 
+export const getStaticProps: GetStaticProps = async () =>  {
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'publisched_at',
+      _order: 'desc'
+    }
+  }) //por causa da ferramenta axios coloca o http://localhost como base saindo ele daqui
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+} 
