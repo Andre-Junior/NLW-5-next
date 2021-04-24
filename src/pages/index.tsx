0d7e0f650b27be206627557c1fa92cc875abc334
Andre-Junior <andrejunior.dias@gmail.com>
2021-04-23 22:52:31 -0300

    base home lastet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,65 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+import styles from './home.module.scss';
+
 //tipagem Types
-type Episode = Array <{
-    id: string;
-    title: string;
-    members: string;
-    published_at: string;
+type Episode = Array<{
+  id: string,
+  title: string,
+  thumbnail: string,
+  members: string,
+  publishedAt: string,
+  duration: number,
+  durationAsString: string,
+  description: string,
+  url: string,
 }> //precisa passar qual o formato de oq é 
 
-
 type HomeProps = {
-  episodes: Episode[]; //outra forma de declarar array
+  latestEpisodes: Episode[]; //outra forma de declarar array
+  allEpisodes: Episode[];
+
 }
 
-
-
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1> 
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <img src={episode.thumbnail} alt={episode.title}/>
+
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+                
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
   )
 }
 
 
 //Next 
-export const getStaticProps: GetStaticProps = async () =>  {
-  const {data} = await api.get('episodes', {
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
     params: {
       _limit: 12,
       _sort: 'publisched_at',
@@ -40,14 +71,13 @@ export const getStaticProps: GetStaticProps = async () =>  {
   }) //por causa da ferramenta axios coloca o http://localhost como base saindo ele daqui e aparecendo os params
 
   //formatação dos dados
-
   const episodes = data.map(episode => {
     return {
       id: episode.id,
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}), //Formatação através do date-fns
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }), //Formatação através do date-fns
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
@@ -56,11 +86,15 @@ export const getStaticProps: GetStaticProps = async () =>  {
     }
   })
 
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.lenght)
+
 
   return {
     props: {
-      episodes,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   }
-} 
+}
